f5128abca5c088ea28e245decd9b6ad2164cfba4
SiliasOdion809 <siliasodion809@gmail.com>
2026-05-15 20:51:47 +0100

    update the output.tf document

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,21 @@ output "s3_website_url" {
 output "redis_endpoint" {
   value = aws_elasticache_cluster.redis_cluster.cache_nodes[0].address
 }
+
+output "load_balancer_dns" {
+  description = "Application Load Balancer DNS Name"
+
+  value = aws_lb.application_load_balancer.dns_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront Distribution ID"
+
+  value = aws_cloudfront_distribution.frontend_cdn.id
+}
+
+output "s3_bucket_name" {
+  description = "Frontend S3 Bucket Name"
+
+  value = aws_s3_bucket.frontend_bucket.bucket
+}
